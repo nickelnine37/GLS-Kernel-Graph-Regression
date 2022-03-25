e24eee8cec228af77049d5c2adec70d5f55cd08b
nickelnine37 <edward.antonian1@gmail.com>
2022-03-25 10:14:46 +0000

    added graph features model

--- model/features.py
+++ model/features.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import os
 import numpy as np
+from scipy.sparse.csgraph import minimum_spanning_tree
 from sklearn.decomposition import PCA
 from scipy.spatial.distance import pdist, squareform
+
+from model.regression.base import filter_functions
 
 CURRENT_FOLDER = os.path.dirname(os.path.realpath(__file__))
 
@@ -19,6 +22,7 @@ class Features:
         self.metrics = ['Ozone', 'SO2', 'CO', 'NO2', 'PM25', 'PM10', 'Wind', 'Pressure', 'Temperature', 'Humidity']
         self.X = self.get_X()
         self.diff = squareform(pdist(self.X.values))
+        self.edges = self.get_edges()
 
     def get_X(self) -> pd.DataFrame:
         """
@@ -36,6 +40,43 @@ class Features:
         Get a (T, T) numpy array representing the exponential kernel matrix
         """
         return np.exp(-self.diff / ss)
+
+    def get_L(self):
+        """
+        Get the features Laplacian
+        """
+
+        xi, yi = np.array(self.edges).T
+        A = np.zeros_like(self.diff)
+        A[xi, yi] = 1
+        A[yi, xi] = 1
+
+        self.L = np.diag(A.sum(0)) - A
+
+        self.lamL, self.U = np.linalg.eigh(self.L)
+
+        return self.L
+
+    def get_Hs(self, filter_function: str='exponential', beta: float=1):
+
+        self.Hs =  (self.U * filter_functions[filter_function](self.lamL, beta) ** 2) @ self.U.T
+        return self.Hs
+
+    def get_edges(self, n_retries: int=1, sigma_D: float=5, seed: int=0) -> list:
+        """
+        Use perturbed MST algorithm to find sparse graph. Return edges as list of (i, j) tuples
+        """
+
+        np.random.seed(seed)
+
+        mst = minimum_spanning_tree(self.diff)
+        edges = set(tuple(i) for i in np.argwhere(mst))
+
+        for i in range(n_retries):
+            mst = minimum_spanning_tree(self.diff + np.random.normal(loc=0, scale=sigma_D, size=self.diff.shape))
+            edges = edges.union(set(tuple(i) for i in np.argwhere(mst)))
+
+        return list(edges)
 
     @staticmethod
     def get_data_PCA(metric: str, transform: str, n_components=10 ) -> pd.DataFrame:
